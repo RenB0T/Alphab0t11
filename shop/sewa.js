0784744a448eecd823ebfd5b4ebe7268b5110296
RenB0T <94209612+RenB0T@users.noreply.github.com>
2021-11-23 03:09:29 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -13,16 +13,16 @@ return `「 *PRICE LIST* 」
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 35K
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285845823026
 
 
 NOTE : 
 Bot On 24 Jam Karna Di Run Menggunakan Heroku
-Reset Data Setiap Jam 02:00 `
+Reset Data Setiap Jam 06:00-12:00 / 18:00-23:00 `
 	}
 
 exports.pc_sewa = pc_sewa
