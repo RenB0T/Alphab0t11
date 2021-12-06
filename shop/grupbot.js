9e31783d027198b992b558917bbe4dad49c1cc53
RenB0T <94209612+RenB0T@users.noreply.github.com>
2021-12-06 19:21:05 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,13 +2,13 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Offc RenB0T-1 🤖*
+1. *Offc RenB0T 🤖*
 _https://chat.whatsapp.com/Ia1iqqUnchtCqmpxb17kSm_
 2. *Anime_Lover`s*
 _https://chat.whatsapp.com/KtHfhdyNCqv0JKIjKhssu1_
 3. *9 Oktober 📌*
 _https://chat.whatsapp.com/DcvnYkG32sx24CRWtXw489_
-4. *Offc RenB0T-2 🤖* 
+4. *Offc RenB0T 🤖* 
 _https://chat.whatsapp.com/BeirAfnkfBoE0bogjbtWcr_
 
 Jika ada link yang ke reset, silahkan hubungi
